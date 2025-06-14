2564aa3a656f04d79c5d91fd7135d0fc438e8bc4
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-06-13 22:43:57 -0700

    cleanup

--- main.go
+++ main.go
@@ -85,7 +85,7 @@ func startAction(ctx context.Context, cmd *cli.Command) error {
 	fset, f := parser2(string(by))
 	i := NewInferrer(fset)
 	i.InferFile(f)
-	fmt.Println(codegen(fset, i.env, f))
+	fmt.Println(codegen(i.env, f))
 	return nil
 }
 
