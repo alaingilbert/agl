9b0d013fb3b534010b21547d7026d7c10456562b
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-06-14 15:24:52 -0700

    build cmd

--- main.go
+++ main.go
@@ -54,6 +54,12 @@ func main() {
 				Usage:   "run command",
 				Action:  runAction,
 			},
+			{
+				Name:    "build",
+				Aliases: []string{"b"},
+				Usage:   "build command",
+				Action:  buildAction,
+			},
 		},
 		Action: startAction,
 	}
@@ -96,9 +102,6 @@ func runAction(ctx context.Context, cmd *cli.Command) error {
 		return nil
 	}
 	fileName := cmd.Args().Get(0)
-	if fileName == "run" {
-		return runAction(ctx, cmd)
-	}
 	if !strings.HasSuffix(fileName, ".agl") {
 		fmt.Println("file must have '.agl' extension")
 		return nil
@@ -112,6 +115,30 @@ func runAction(ctx context.Context, cmd *cli.Command) error {
 	i.InferFile(f)
 	src := NewGenerator(i.env, f).Generate()
 	_ = spawnGoRunFromBytes([]byte(src))
+	return nil
+}
+
+func buildAction(ctx context.Context, cmd *cli.Command) error {
+	if cmd.NArg() == 0 {
+		fmt.Println("You must specify a file to compile")
+		return nil
+	}
+	fileName := cmd.Args().Get(0)
+	if !strings.HasSuffix(fileName, ".agl") {
+		fmt.Println("file must have '.agl' extension")
+		return nil
+	}
+	by, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	fset, f := parser2(string(by))
+	i := NewInferrer(fset)
+	i.InferFile(f)
+	src := NewGenerator(i.env, f).Generate()
+	if err := os.WriteFile(strings.Replace(fileName, ".agl", ".go", 1), []byte(src), 0644); err != nil {
+		return err
+	}
 	return nil
 }
 
