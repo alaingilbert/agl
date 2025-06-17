ba494049ce9c093543771068950eb9f5e7b9b3cd
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-06-16 20:57:08 -0700

    fix ellipsis string

--- pkg/agl/env_test.go
+++ pkg/agl/env_test.go
@@ -11,3 +11,9 @@ func Test1(t *testing.T) {
 	tt := env.Get("http.NewRequest")
 	tassert.Equal(t, "func http.NewRequest(string, string, io.Reader) Result[*Request]", tt.String())
 }
+
+func Test2(t *testing.T) {
+	env := NewEnv(nil)
+	tt := env.Get("fmt.Println")
+	tassert.Equal(t, "func fmt.Println(...any) Result[int]", tt.String())
+}
