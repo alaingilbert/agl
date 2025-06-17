b324e972949bb8adb152c49d1a586f016f717e06
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-06-16 22:10:25 -0700

    improve string

--- pkg/agl/env_test.go
+++ pkg/agl/env_test.go
@@ -9,11 +9,11 @@ import (
 func Test1(t *testing.T) {
 	env := NewEnv(nil)
 	tt := env.Get("http.NewRequest")
-	tassert.Equal(t, "func http.NewRequest(string, string, io.Reader) Result[*Request]", tt.String())
+	tassert.Equal(t, "func NewRequest(string, string, io.Reader) Result[*Request]", tt.String())
 }
 
 func Test2(t *testing.T) {
 	env := NewEnv(nil)
 	tt := env.Get("fmt.Println")
-	tassert.Equal(t, "func fmt.Println(...any) Result[int]", tt.String())
+	tassert.Equal(t, "func Println(...any) Result[int]", tt.String())
 }
