e69d24b4b61eedc578aa9d761bb7a239d7e68688
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-06-18 11:37:08 -0700

    add go tour example to tests

--- pkg/agl/codegen_test.go
+++ pkg/agl/codegen_test.go
@@ -4711,6 +4711,69 @@ func main() {
 	testCodeGen(t, src, expected)
 }
 
+func TestCodeGen178(t *testing.T) {
+	src := `package main
+import (
+	"fmt"
+	"math"
+)
+type I interface {
+	M()
+}
+type T struct {
+	S string
+}
+func (t *T) M() {
+	fmt.Println(t.S)
+}
+type F f64
+func (f F) M() {
+	fmt.Println(f)
+}
+func main() {
+	var i I
+	i = &T{"Hello"}
+	describe(i)
+	i.M()
+	i = F(math.Pi)
+	describe(i)
+	i.M()
+}
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}`
+	expected := `package main
+import "fmt"
+import "math"
+type I interface {
+	M()
+}
+type T struct {
+	S string
+}
+func (t *T) M() {
+	fmt.Println(t.S)
+}
+type F float64
+func (f F) M() {
+	fmt.Println(f)
+}
+func main() {
+	var i I
+	i = &T{"Hello"}
+	describe(i)
+	i.M()
+	i = F(math.Pi)
+	describe(i)
+	i.M()
+}
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
+`
+	testCodeGen(t, src, expected)
+}
+
 //func TestCodeGen167(t *testing.T) {
 //	src := `package main
 //func test(t (u8, bool)) (u8, bool) { return t }
