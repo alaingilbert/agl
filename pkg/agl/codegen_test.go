42a8e0a4d73b027e19c0f8f1d91db6ceb04d4c84
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-06-18 18:11:17 -0700

    handle unary expr

--- pkg/agl/codegen_test.go
+++ pkg/agl/codegen_test.go
@@ -5308,6 +5308,45 @@ func main() {
 	testCodeGen(t, src, expected)
 }
 
+func TestCodeGen193(t *testing.T) {
+	src := `package main
+import "fmt"
+func sum(s []int, c chan int) {
+	sum1 := 0
+	for _, v := range s {
+		sum1 += v
+	}
+	c <- sum1 // send sum to c
+}
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // receive from c
+	fmt.Println(x, y, x+y)
+}`
+	expected := `package main
+import "fmt"
+func sum(s []int, c chan int) {
+	sum1 := 0
+	for _, v := range s {
+		sum1 += v
+	}
+	c <- sum1
+}
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+	c := make(chan int)
+	go sum(s[:len(s) / 2], c)
+	go sum(s[len(s) / 2:], c)
+	x, y := <-c, <-c
+	fmt.Println(x, y, x + y)
+}
+`
+	testCodeGen(t, src, expected)
+}
+
 //func TestCodeGen167(t *testing.T) {
 //	src := `package main
 //func test(t (u8, bool)) (u8, bool) { return t }
