f09176ea9afe3e42fb3304d16ee795f04d6433b9
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-06-18 20:44:14 -0700

    add time fns

--- pkg/agl/codegen_test.go
+++ pkg/agl/codegen_test.go
@@ -5393,6 +5393,59 @@ func main() {
 	testCodeGen(t, src, expected)
 }
 
+func TestCodeGen195(t *testing.T) {
+	src := `package main
+import (
+	"fmt"
+	"time"
+)
+func main() {
+	start := time.Now()
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	elapsed := func() time.Duration {
+		return time.Since(start).Round(time.Millisecond)
+	}
+	for {
+		select {
+		case <-tick:
+			fmt.Printf("[%6s] tick.\n", elapsed())
+		case <-boom:
+			fmt.Printf("[%6s] BOOM!\n", elapsed())
+			return
+		default:
+			fmt.Printf("[%6s]     .\n", elapsed())
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}`
+	expected := `package main
+import "fmt"
+import "time"
+func main() {
+	start := time.Now()
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	elapsed := func() time.Duration {
+		return time.Since(start).Round(time.Millisecond)
+	}
+	for {
+		select {
+		case <-tick:
+		fmt.Printf("[%6s] tick.\n", elapsed())
+		case <-boom:
+		fmt.Printf("[%6s] BOOM!\n", elapsed())
+		return
+		default:
+		fmt.Printf("[%6s]     .\n", elapsed())
+		time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
+`
+	testCodeGen(t, src, expected)
+}
+
 //func TestCodeGen167(t *testing.T) {
 //	src := `package main
 //func test(t (u8, bool)) (u8, bool) { return t }
