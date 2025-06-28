ab57f01248b085eafabc9669ab8b0555d664b236
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-06-27 22:37:11 -0400

    Add comment
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- main.go
+++ main.go
@@ -312,6 +312,7 @@ func startAction(ctx context.Context, cmd *cli.Command) error {
 }
 
 func parser2(src string) (*token.FileSet, *ast.File) {
+	// support "#!/usr/bin/env agl run" as first line of agl "script"
 	if strings.HasPrefix(src, "#!") {
 		src = "//" + src
 	}
