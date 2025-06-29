190d638e0a0a9f3a9248b3056a8b7b347762df49
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-06-28 23:13:05 -0700

    add agl.String.I8

--- pkg/agl/infer.go
+++ pkg/agl/infer.go
@@ -1040,6 +1040,12 @@ func (infer *FileInferrer) inferGoExtensions(expr *ast.CallExpr, idT types.Type,
 			fnT.Params = fnT.Params[1:]
 			infer.SetType(exprT.Sel, fnT)
 			infer.SetType(expr, fnT.Return)
+		} else if fnName == "I8" {
+			fnT := infer.env.GetFn("agl.String.I8")
+			fnT.Recv = []types.Type{idTT}
+			fnT.Params = fnT.Params[1:]
+			infer.SetType(exprT.Sel, fnT)
+			infer.SetType(expr, fnT.Return)
 		} else if fnName == "F64" {
 			fnT := infer.env.GetFn("agl.String.F64")
 			fnT.Recv = []types.Type{idTT}
