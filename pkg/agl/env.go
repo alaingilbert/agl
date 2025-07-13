9d902621dfc5d993f760f401a4c2aa06e0c156c1
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-07-13 00:22:38 -0700

    fix GetType for CompositeLit MapType

--- pkg/agl/env.go
+++ pkg/agl/env.go
@@ -1253,6 +1253,8 @@ func (e *Env) getType2Helper(x ast.Node, fset *token.FileSet) types.Type {
 			return types.ArrayType{Elt: e.GetType2(xx.Type, fset)}
 		case types.StructType:
 			return types.StructType{Pkg: v.Pkg, Name: v.Name}
+		case types.MapType:
+			return e.GetType2(xx.Type, fset)
 		default:
 			//return nil
 			panic(fmt.Sprintf("%v %v", xx.Type, reflect.TypeOf(v)))
