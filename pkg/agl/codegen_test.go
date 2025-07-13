f3accb08314f0ba2d395ed2c18de94e85e248ff0
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-07-12 23:58:07 -0700

    add test to ensure Sorted stays good

--- pkg/agl/codegen_test.go
+++ pkg/agl/codegen_test.go
@@ -9264,6 +9264,23 @@ func main() {
 	testCodeGen1(t, test.GenCode(), expected)
 }
 
+func TestCodeGen320(t *testing.T) {
+	src := `package main
+func main() {
+	a := []int{1, 2, 3}.Sorted()
+}`
+	expected := `// agl:generated
+package main
+func main() {
+	a := AglVecSorted([]int{1, 2, 3})
+}
+`
+	test := NewTest(src, WithMutEnforced(true))
+	tassert.Equal(t, "[]int", test.TypeAt(3, 2).String())
+	tassert.Equal(t, 0, len(test.errs))
+	testCodeGen1(t, test.GenCode(), expected)
+}
+
 //func TestCodeGen318(t *testing.T) {
 //	src := "" +
 //		"package main\n" +
