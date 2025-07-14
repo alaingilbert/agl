a60d8ab1f056692ba56f090110d68a8865a5cc3c
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-07-14 09:34:45 -0700

    cleanup

--- pkg/agl/codegen_test.go
+++ pkg/agl/codegen_test.go
@@ -9574,14 +9574,11 @@ type MyType struct {
 }
 func (m MyType) Iter() iter.Seq[int] {
 	return func(yield func(int) bool) {
-		if !yield(m.a) {
-			return
-		}
-		if !yield(m.b) {
-			return
-		}
-		if !yield(m.c) {
-			return
+		vals := []int{m.a, m.b, m.c}
+		for _, el := range vals {
+			if !yield(el) {
+				return
+			}	
 		}
 	}
 }`
@@ -9593,14 +9590,11 @@ type MyType struct {
 }
 func (m MyType) Iter() iter.Seq[int] {
 	return func(yield func(int) bool) {
-		if !yield(m.a) {
-			return
-		}
-		if !yield(m.b) {
-			return
-		}
-		if !yield(m.c) {
-			return
+		vals := []int{m.a, m.b, m.c}
+		for _, el := range vals {
+			if !yield(el) {
+				return
+			}
 		}
 	}
 }
