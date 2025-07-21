a2f461700ee4f476fe153605fa66cb3d8b00d4e1
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-07-20 19:08:08 -0700

    AglIntger VS AglNumber

--- pkg/agl/codegen.go
+++ pkg/agl/codegen.go
@@ -3997,10 +3997,13 @@ func AglVecSum[T aglImportCmp.Ordered](a []T) (out T) {
 	return AglVecReduce(a, zero, func(acc, el T) T { return acc + el })
 }
 
-type AglNumber interface {
+type AglInteger interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
-		~float32 | ~float64
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+type AglNumber interface {
+	AglInteger | ~float32 | ~float64
 }
 
 func AglAbs[T AglNumber](e T) (out T) {
