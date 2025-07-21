284a9d64e2fd71b56038aab6df3d26e8764380d4
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-07-20 19:32:38 -0700

    cleanup

--- pkg/agl/codegen.go
+++ pkg/agl/codegen.go
@@ -3997,13 +3997,28 @@ func AglVecSum[T aglImportCmp.Ordered](a []T) (out T) {
 	return AglVecReduce(a, zero, func(acc, el T) T { return acc + el })
 }
 
-type AglInteger interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
 }
 
-type AglNumber interface {
-	AglInteger | ~float32 | ~float64
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+type Integer interface {
+	Signed | Unsigned
+}
+
+type Float interface {
+	~float32 | ~float64
+}
+
+type Complex interface {
+	~complex64 | ~complex128
+}
+
+type Number interface {
+	Integer | Float
 }
 
 func AglAbs[T AglNumber](e T) (out T) {
