c4db7ad8dd68f31477351c50afa75918093d5f50
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-07-23 13:02:18 -0700

    typo

--- pkg/agl/env.go
+++ pkg/agl/env.go
@@ -852,7 +852,7 @@ func (e *Env) loadPkgAgl(m *PkgVisited) {
 		e.DefineFn(nenv, "agl1.Set.Remove", "func [T comparable](mut s agl1.Set[T], el T) T?")
 		e.DefineFn(nenv, "agl1.Set.Contains", "func [T comparable](s agl1.Set[T], el T) bool")
 		e.DefineFn(nenv, "agl1.Set.Iter", "func [T comparable](s agl1.Set[T]) Sequence[T]")
-		e.DefineFn(nenv, "agl1.Set.Filter", "func [T comparable](a agl1.Set[T], f func(e T) bool) agl1.Set[T]", WithDesc("Returns a new set containing the elements of the set that satisfy the given predicate."))
+		e.DefineFn(nenv, "agl1.Set.Filter", "func [T comparable](s agl1.Set[T], f func(e T) bool) agl1.Set[T]", WithDesc("Returns a new set containing the elements of the set that satisfy the given predicate."))
 		e.DefineFn(nenv, "agl1.Set.Union", "func [T comparable](s agl1.Set[T], other Iterator[T]) agl1.Set[T]")
 		e.DefineFn(nenv, "agl1.Set.FormUnion", "func [T comparable](mut s agl1.Set[T], other Iterator[T])")
 		e.DefineFn(nenv, "agl1.Set.Subtracting", "func [T comparable](s agl1.Set[T], other Iterator[T]) agl1.Set[T]")
