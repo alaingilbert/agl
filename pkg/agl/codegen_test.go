e50becb65b65875a06559e2af90f82568d9f386e
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-10-07 16:29:08 -0700

    add test

--- pkg/agl/codegen_test.go
+++ pkg/agl/codegen_test.go
@@ -13534,6 +13534,24 @@ func main() {
 	testCodeGen2(t, expected, test)
 }
 
+func TestCodeGen440(t *testing.T) {
+	src := `package main
+func main() {
+	s := "-"
+	assert(s.Repeat(5) == "-----")
+}`
+	expected := `// agl:generated
+package main
+func main() {
+	s := "-"
+	AglAssert(AglStringRepeat(s, 5) == "-----", "assert failed line 4")
+}
+`
+	test := NewTest(src, WithMutEnforced(true))
+	tassert.Equal(t, 0, len(test.errs))
+	testCodeGen2(t, expected, test)
+}
+
 //func TestCodeGen411(t *testing.T) {
 //	src := `package main
 //func main() {
