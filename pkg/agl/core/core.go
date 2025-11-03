d4807b5a64b9ffc71e6eda7fc2c90ca3ff27d664
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-11-02 19:30:01 -0800

    fix noop

--- pkg/agl/core/core.go
+++ pkg/agl/core/core.go
@@ -336,7 +336,7 @@ func AglVecIn[T comparable](a []T, v T) bool {
 	return false
 }
 
-func AglNoop(_ ...any) Result[AglVoid] { return MakeResultOk(AglVoid{}) }
+func AglNoop(_ ...any) {}
 
 func AglTypeAssert[T any](v any) Option[T] {
 	if v, ok := v.(T); ok {
