f5d595a68b455a192134b9ef58cbb0033a5deffb
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-11-05 12:08:38 -0800

    add AglSequenceFlatMap & AglSequenceCompactMap to core

--- pkg/agl/core/core.go
+++ pkg/agl/core/core.go
@@ -423,6 +423,32 @@ func AglSequenceFilterMap[T, R any](s Sequence[T], f func(T) Option[R]) Sequence
 	}
 }
 
+func AglSequenceFlatMap[T, R any](s Sequence[T], f func(T) []R) Sequence[R] {
+	return func(yield func(R) bool) {
+		for el := range s {
+			subArr := f(el)
+			for _, el1 := range subArr {
+				if !yield(el1) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func AglSequenceCompactMap[T, R any](s Sequence[T], f func(T) Option[R]) Sequence[R] {
+	return func(yield func(R) bool) {
+		for el := range s {
+			res := f(el)
+			if res.IsSome() {
+				if !yield(res.Unwrap()) {
+					return
+				}
+			}
+		}
+	}
+}
+
 func AglSequenceFilter[T any](s Sequence[T], f func(T) bool) Sequence[T] {
 	return func(yield func(T) bool) {
 		for v := range s {
