76dd18b66ab9be3e906133c011fc4da10837c49b
Alain Gilbert <alain.gilbert.15@gmail.com>
2025-11-07 12:53:46 -0800

    fix signature

--- pkg/agl/env.go
+++ pkg/agl/env.go
@@ -1100,7 +1100,7 @@ func (e *Env) loadPkgAgl(m *PkgVisited) {
 		e.DefineFn(nenv, "agl1.Iterator.TakeWhile", "func [T any](it Iterator[T], f func(T) bool) Iterator[T]")
 		e.DefineFn(nenv, "agl1.Iterator.Map", "func [T, R any](it Iterator[T], f func(T) R) Iterator[R]")
 		e.DefineFn(nenv, "agl1.Iterator.Count", "func [T any](it Iterator[T]) int")
-		e.DefineFn(nenv, "agl1.Iterator.Cycle", "func [T any](it Iterator[T]) Iterator[T]")
+		e.DefineFn(nenv, "agl1.Iterator.Cycle", "func [T any](it CloneIterator[T]) Iterator[T]")
 	})
 }
 
